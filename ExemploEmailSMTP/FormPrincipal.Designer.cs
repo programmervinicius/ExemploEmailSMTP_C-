840e86c57d3ac5e90b8135a746e9e78485689feb
viniciusldemelo@gmail.com <viniciusldemelo@gmail.com>
2016-06-04 16:44:19 -0300

    Primeiro commit

---
+++ ExemploEmailSMTP/FormPrincipal.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace ExemploEmailSMTP
+{
+    partial class FormTesteEnvioEmail
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEnviarEmailTesteSincrono = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnEnviarEmailTesteSincrono
+            // 
+            this.btnEnviarEmailTesteSincrono.Location = new System.Drawing.Point(119, 68);
+            this.btnEnviarEmailTesteSincrono.Margin = new System.Windows.Forms.Padding(2);
+            this.btnEnviarEmailTesteSincrono.Name = "btnEnviarEmailTesteSincrono";
+            this.btnEnviarEmailTesteSincrono.Size = new System.Drawing.Size(156, 55);
+            this.btnEnviarEmailTesteSincrono.TabIndex = 0;
+            this.btnEnviarEmailTesteSincrono.Text = "Enviar E-mail de Teste";
+            this.btnEnviarEmailTesteSincrono.UseVisualStyleBackColor = true;
+            this.btnEnviarEmailTesteSincrono.Click += new System.EventHandler(this.btnEnviarEmailTesteSincrono_Click);
+            // 
+            // FormTesteEnvioEmail
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(415, 206);
+            this.Controls.Add(this.btnEnviarEmailTesteSincrono);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximizeBox = false;
+            this.Name = "FormTesteEnvioEmail";
+            this.Text = "Teste de Envio de E-mail";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEnviarEmailTesteSincrono;
+    }
+}
+
---
+++ ExemploEmailSMTP/FormPrincipal.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ExemploEmailSMTP
+{
+    public partial class FormTesteEnvioEmail : Form
+    {
+        public FormTesteEnvioEmail()
+        {
+            InitializeComponent();
+        }
+
+        private void enviarEmailTesteSincrono()
+        {
+            string sHost = "smtp.gmail.com", sEmail = "exemploemailsmtp@gmail.com", sSenha = "senha001#";
+            try
+            {
+                using (System.Net.Mail.SmtpClient smtp = new System.Net.Mail.SmtpClient())
+                {
+                    using (System.Net.Mail.MailMessage mail = new System.Net.Mail.MailMessage())
+                    {
+                        smtp.Host = sHost;
+                        smtp.Port = 587;
+                        smtp.EnableSsl = true;
+                        smtp.UseDefaultCredentials = false;
+                        smtp.Credentials = new System.Net.NetworkCredential(sEmail, sSenha);
+
+                        mail.From = new System.Net.Mail.MailAddress(sEmail);
+                        mail.To.Add(new System.Net.Mail.MailAddress(sEmail));
+                        //mail.CC.Add(new System.Net.Mail.MailAddress(sEmail));
+                        //mail.Bcc.Add(new System.Net.Mail.MailAddress(sEmail));
+                        mail.Subject = "Assunto do e-mail de teste.";
+                        mail.Body = "Corpo da mensagem do e-mail de teste.";
+
+                        // Anexar arquivos                        
+                        //using (OpenFileDialog dialog = new OpenFileDialog())
+                        //{
+                        //    dialog.Multiselect = true;
+                        //    if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                        //    {
+                        //        foreach (var file in dialog.FileNames)
+                        //        {
+                        //            listBoxAttachments.Items.Add(file);
+                        //        }
+                        //    }
+                        //}                                        
+                        //foreach (string file in listBoxAttachments.Items)
+                        //{
+                        //    mail.Attachments.Add(new System.Net.Mail.Attachment(file));
+                        //}
+                      
+                        // Formatação do texto para HTML
+                        //mail.IsBodyHtml = true;
+                        //mail.Body = MarkupConverter.RtfToHtmlConverter.ConvertRtfToHtml(richTextBoxCorpo.Rtf); 
+                        
+                        smtp.Send(mail);
+                        Informacao("E-mail de teste enviado com sucesso.");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Erro("Erro ao enviar e-mail de teste.", ex);
+            }
+        }
+
+        public static void Erro(string Msg, Exception ex)
+        {
+            MessageBox.Show(Msg + "\n" + ex.Message + "\n" + ex.InnerException, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Hand, MessageBoxDefaultButton.Button1);
+        }
+
+        public static void Informacao(string Msg)
+        {
+            MessageBox.Show(Msg + "\n", "Informação", MessageBoxButtons.OK, MessageBoxIcon.Information, MessageBoxDefaultButton.Button1);
+        }
+
+        // Envia um e-mail de teste assíncrono. Atenção: Válido apenas usando o Visual Studio 2012 
+        // para a área de trabalho do Windows, ou .NET Framework 4.5 instalado no seu computador.
+        /*
+        private async void enviarEmailTesteAssincrono()
+        {
+            string sHost = "smtp.gmail.com", sEmail = "exemploemailsmtp@gmail.com", sSenha = "senha001#";
+            try
+            {
+                using (System.Net.Mail.SmtpClient smtp = new System.Net.Mail.SmtpClient())
+                {
+                    using (System.Net.Mail.MailMessage mail = new System.Net.Mail.MailMessage())
+                    {
+                        smtp.Host = sHost;
+                        smtp.Port = 587;
+                        smtp.EnableSsl = true;
+                        smtp.UseDefaultCredentials = false;
+                        smtp.Credentials = new System.Net.NetworkCredential(sEmail, sSenha);
+
+                        mail.From = new System.Net.Mail.MailAddress(sEmail);
+                        mail.To.Add(new System.Net.Mail.MailAddress(sEmail));
+                        //mail.CC.Add(new System.Net.Mail.MailAddress(sEmail));
+                        //mail.Bcc.Add(new System.Net.Mail.MailAddress(sEmail));
+                        mail.Subject = "Assunto do e-mail de teste.";
+                        mail.Body = "Corpo da mensagem do e-mail de teste.";
+
+                        // Anexar arquivos                        
+                        //using (OpenFileDialog dialog = new OpenFileDialog())
+                        //{
+                        //    dialog.Multiselect = true;
+                        //    if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                        //    {
+                        //        foreach (var file in dialog.FileNames)
+                        //        {
+                        //            listBoxAttachments.Items.Add(file);
+                        //        }
+                        //    }
+                        //}                                        
+                        //foreach (string file in listBoxAttachments.Items)
+                        //{
+                        //    mail.Attachments.Add(new System.Net.Mail.Attachment(file));
+                        //}
+                      
+                        // Formatação do texto para HTML
+                        //mail.IsBodyHtml = true;
+                        //mail.Body = MarkupConverter.RtfToHtmlConverter.ConvertRtfToHtml(richTextBoxCorpo.Rtf); 
+                        
+                        await smtp.SendMailAsync(mail);
+                        Informacao("E-mail de teste enviado com sucesso.");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Erro("Erro ao enviar e-mail de teste.", ex);
+            }           
+        }
+        */
+
+        private void btnEnviarEmailTesteSincrono_Click(object sender, EventArgs e)
+        {
+            enviarEmailTesteSincrono();
+        }
+    }
+}
